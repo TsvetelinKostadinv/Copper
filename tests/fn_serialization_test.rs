33abbefc77de7bcfe8fcaddf7717fb98400c159e
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2020-12-04 11:41:10 +0200

    Removed unnecessary imports and comments

--- tests/fn_serialization_test.rs
+++ tests/fn_serialization_test.rs
@@ -1,8 +1,5 @@
-// use serde::{Deserialize, Serialize};
 use serde::{Deserialize, Serialize};
-use serde_closure::Fn;
 use serde_traitobject as s;
-use std::any::Any;
 
 trait Executable<Args: s::Serialize + s::Deserialize, Res: s::Serialize + s::Deserialize>:
     s::Serialize + s::Deserialize
@@ -26,11 +23,11 @@ fn executable_after_serialization() {
     let erased: s::Box<dyn s::Any> = s::Box::new(printer);
     let serialized = serde_json::to_string(&erased).expect("Unable to serialize the function!");
 
-    let deserialized: s::Box< dyn s::Any > =
+    let deserialized: s::Box<dyn s::Any> =
         serde_json::from_str(&serialized).expect("Unable to deserialize the function!");
 
     let downcast: Box<Printer> =
-        Box::<dyn Any>::downcast(deserialized.into_any()).expect("Unable to downcast");
+        Box::<dyn std::any::Any>::downcast(deserialized.into_any()).expect("Unable to downcast");
 
     (&downcast).exec(());
 }
@@ -38,10 +35,8 @@ fn executable_after_serialization() {
 #[derive(Serialize, Deserialize)]
 struct Incrementer;
 
-impl Executable<(i32,i32), i32> for Incrementer
-{
-    fn exec(&self, args:(i32,i32)) -> i32
-    {
+impl Executable<(i32, i32), i32> for Incrementer {
+    fn exec(&self, args: (i32, i32)) -> i32 {
         args.0 + args.1
     }
 }
@@ -53,11 +48,11 @@ fn executable_after_serialization_with_args() {
     let erased: s::Box<dyn s::Any> = s::Box::new(incrementer);
     let serialized = serde_json::to_string(&erased).expect("Unable to serialize the function!");
 
-    let deserialized: s::Box< dyn s::Any > =
+    let deserialized: s::Box<dyn s::Any> =
         serde_json::from_str(&serialized).expect("Unable to deserialize the function!");
 
     let downcast: Box<Incrementer> =
-        Box::<dyn Any>::downcast(deserialized.into_any()).expect("Unable to downcast");
+        Box::<dyn std::any::Any>::downcast(deserialized.into_any()).expect("Unable to downcast");
 
-    assert_eq!(Incrementer.exec((3,5)), (&downcast).exec((3,5)));
+    assert_eq!(Incrementer.exec((3, 5)), (&downcast).exec((3, 5)));
 }
