c78d701a1c9c07b8e3d1392a9f47153dd1f641a7
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2021-02-21 01:24:22 +0200

    Fixing buildscript

--- build.rs
+++ build.rs
@@ -2,8 +2,20 @@
 extern crate winres;
 
 #[cfg(windows)]
-fn main() {
+fn add_icon() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("copper.ico");
     res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn empty() {
+    
+}
+
+fn main() {
+    #[cfg(windows)]
+    add_icon();
+    #[cfg(not(windows))]
+    empty();
 }
